920b108a129a60eda02839a56177881c2262be02
JosephGabrielCheng2009 <Jamesdonny1980@gmail.com>
2025-02-19 01:52:17 +0000

    Submit

---
+++ Year/Year.py
@@ -0,0 +1,13 @@
+user_input=int(input("Enter your birth year "))
+if 1928<= user_input <=1945:
+    print("You belong to Silent Generation")
+elif 1946<= user_input <=1964:
+    print("You belong to Baby Boomer")
+elif 1965<= user_input <=1980:
+    print("You belong to Generation X")
+elif 1981<= user_input <=1996:
+    print("You belong to Millenials")
+elif 1997<= user_input <=2012:
+    print("You belong to Generation Z")
+elif 2013<= user_input <= 2025:
+    print("You belong to Generation Alpha")
